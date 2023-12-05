04ab57bbe949046874b4e61df5261447efb5699a
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-12-05 01:19:50 +0100

    testing

--- cmake/buildDocs.cmake
+++ cmake/buildDocs.cmake
@@ -1,4 +1,6 @@
-if(CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME)
+
+find_package(Doxygen)
+if(DOXYGEN_FOUND)
 	file(DOWNLOAD "https://upload.cppreference.com/mwiki/images/f/f8/cppreference-doxygen-web.tag.xml"
 		"${CMAKE_CURRENT_BINARY_DIR}/doxtags/cppreference-doxygen-web.tag.xml")
 	file(DOWNLOAD "https://doc.qt.io/qt-6/qtcore.tags"
@@ -8,7 +10,6 @@ if(CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME)
 	file(DOWNLOAD "https://doc.qt.io/qt-6/qtwebsockets.tags"
 		"${CMAKE_CURRENT_BINARY_DIR}/doxtags/qtwebsockets.tags")
 
-	find_package(Doxygen REQUIRED)
 	get_target_property(doxygen_style css_doxygen_style SOURCES)
 	get_target_property(doxygen_head css_doxygen_Header SOURCES)
 	get_target_property(doxygen_foot css_doxygen_Footer SOURCES)
@@ -34,16 +35,7 @@ if(CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME)
 
 	doxygen_add_docs(
 		doxygen_docs
-		${PROJECT_SOURCE_DIR}/README.md
-		${PROJECT_SOURCE_DIR}/include
-		${PROJECT_SOURCE_DIR}/qml
-		${DOC_SOURCE_DIRS}
+		${PROJECT_SOURCE_DIR}
 		COMMENT "Generate man pages"
 		)
-else()
-	list(APPEND DOC_SOURCE_DIRS ${PROJECT_SOURCE_DIR}/include)
-	list(REMOVE_DUPLICATES DOC_SOURCE_DIRS)
-	set(DOC_SOURCE_DIRS ${DOC_SOURCE_DIRS}  CACHE STRING "" FORCE)
-	mark_as_advanced(DOC_SOURCE_DIRS)
-endif()
-
+endif(DOXYGEN_FOUND)
